c1bd9bc8d9547295dfb6ea33a7bc0c65a7250f96
Narendra S. Inamdar <narendrai1211@gmail.com>
2020-07-27 21:42:44 +0530

    Create count_occurance_of_string.py
    Counts number of occurance of a perticular string inside a pandas data frame column

---
+++ Programming Logic/count_occurance_of_string.py
@@ -0,0 +1,12 @@
+import json
+from collections import Counter
+import pandas as pd
+df = pd.read_csv('file.csv')
+print(df.shape)
+d = df['COL_NAME'].to_list()
+count_each = Counter(d)
+final_dictionary = dict(count_each)
+print(json.dumps(final_dictionary, indent=2))
+
+with open('file.json', 'w') as f:
+    f.write(json.dumps(final_dictionary, indent=2))
