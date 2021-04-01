b1a84c93d8e56e938e286acc8ecadb439d7c55b8
Narendra S Inamdar <narendrai1211@gmail.com>
2021-04-01 14:11:56 +0530

    Added solution for array rotation

---
+++ hackerrank_solutions/array_left_rotation.py
@@ -0,0 +1,27 @@
+
+
+import os
+
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    return a[d:] + a[:d]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
