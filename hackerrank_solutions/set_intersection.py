44fcb6479153fe297198153b3f4e0caf901e9b0e
Narendra S Inamdar <narendrai1211@gmail.com>
2021-05-31 00:40:42 +0530

    added set intersection

---
+++ hackerrank_solutions/set_intersection.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    len_a = int(input())
+    a = set(input().split())
+    len_b = int(input())
+    b = set(input().split())
+
+    c = a.intersection(b)
+    print(len(c))
+
+
+
+# https://www.hackerrank.com/challenges/py-set-intersection-operation/problem
